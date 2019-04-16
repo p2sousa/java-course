715597cdf1ddbea5ddf04deef7e6038a94b50aaf
Pablo Sousa <pablosousa.ads@gmail.com>
2019-04-16 13:59:16 -0300

    added dependency DotEnv to load and read variables ambient.

--- src/main/java/com/psousa/javajdbc/App.java
+++ src/main/java/com/psousa/javajdbc/App.java
@@ -1,10 +1,17 @@
 package com.psousa.javajdbc;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+
+        Dotenv dotenv = Dotenv.configure()
+            .directory(System.getProperty("user.dir"))
+            .ignoreIfMalformed()
+            .ignoreIfMissing()
+            .load();
     }
 
 }
