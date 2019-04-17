fa82143264415b8f42e90308c9a338df47c8061b
Pablo Sousa <pablosousa.ads@gmail.com>
2019-04-16 21:27:30 -0300

    connect in database using .env configs and JDBC package

--- src/main/java/com/psousa/javajdbc/App.java
+++ src/main/java/com/psousa/javajdbc/App.java
@@ -2,16 +2,52 @@ package com.psousa.javajdbc;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
+        
         Dotenv dotenv = Dotenv.configure()
             .directory(System.getProperty("user.dir"))
             .ignoreIfMalformed()
             .ignoreIfMissing()
             .load();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Loaded");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Failed to Load");
+            e.printStackTrace();
+        }
+
+        Connection connection = null;
+
+        String urlConnector = "jdbc:mysql://";
+
+        urlConnector += dotenv.get("DB_HOST");
+        urlConnector += ":" + dotenv.get("DB_PORT");
+        urlConnector += "/" + dotenv.get("DB_DATABASE");
+        urlConnector += "?useSSL=false";
+
+        try {
+            connection = DriverManager.getConnection(
+                urlConnector,
+                dotenv.get("DB_USER"),
+                dotenv.get("DB_PASSWORD")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (connection != null) {
+            System.out.println("Connected");
+        } else {
+            System.out.println("Connected Failed");
+        }
     }
 
 }
